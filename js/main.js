30d5851f1808cf9a36792066d65684974333cab5
juniortoledo <deendsstudios@gmail.com>
2021-04-27 09:42:17 -0400

    v2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
-console.log('Hello World!');
+const getClass = what => document.querySelector(what)
+
+let tarefas = [{
+  name: 'Exemplo de tarefa',
+  id: Math.floor(Math.random() * 9999)
+}]
+
+const render = () => {
+  getClass(`.lista`)
+    .innerHTML = tarefas.map(e => `
+      <div class="item">
+        <h2>${e.name}</h2>
+        <button onClick="del(${e.id})">Excluir</button>
+      </div>
+    `)
+}
+
+const del = what => {
+  tarefas = tarefas.filter(e => e.id !== what)
+  
+  saveDb()
+  render()
+}
+
+getClass(`.form`)
+  .addEventListener(`submit`, e => {
+    e.preventDefault()
+    
+    tarefas = [...tarefas, {
+      name: getClass(`.input`).value,
+      id: Math.floor(Math.random() * 9999)
+    }]
+    
+    saveDb()
+    render()
+    
+    getClass(`.input`).value = ``
+  })
+
+const saveDb = () => {
+  localStorage.setItem('lista', JSON.stringify(tarefas))
+}
+
+if(localStorage.getItem(`lista`) === null || localStorage.getItem(`lista`) === [] ) {
+  render()
+} else {
+  tarefas = JSON.parse(localStorage.getItem(`lista`))
+  
+  render()
+}
